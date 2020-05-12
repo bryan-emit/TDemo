eb6947ce7042fe06fe33ef80c6074e44daf2092a
bryan-emit <app2bryan@gmail.com>
2020-05-12 22:14:39 +0800

    Create WPR.user.js

---
+++ WPR.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name              Test
+// @description       Test Desc
+// @namespace         https://github.com/
+// @version           7.0.1
+// @match             https://*.azurewebsites.net/*
+// @include           https://*.azurewebsites.net/*
+// @noframes
+// @run-at            document-start
+// @grant             GM.info
+// @grant             GM.xmlHttpRequest
+// @grant             GM.addValueChangeListener
+// @grant             GM.listValues
+// @grant             GM.getValue
+// @grant             GM.setValue
+// @grant             GM.deleteValue
+// @grant             GM.notification
+// @grant             GM.registerMenuCommand
+// @grant             GM_info
+// @grant             GM_xmlhttpRequest
+// @grant             GM_addValueChangeListener
+// @grant             GM_listValues
+// @grant             GM_getValue
+// @grant             GM_setValue
+// @grant             GM_deleteValue
+// @grant             GM_notification
+// @grant             GM_registerMenuCommand
+// @nocompat
+// @author            WPR
+// @license           MPL-2.0
+// @downloadURL       https://github.com/bryan-emit/Test/raw/master/WPR.user.js
+// @homepageURL       https://github.com/
+// ==/UserScript==
+console.log('777.00');
